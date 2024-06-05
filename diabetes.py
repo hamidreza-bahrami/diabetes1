bd0d620796993623ee36393a02ed24082febbff4
hamidreza-bahrami <150763463+hamidreza-bahrami@users.noreply.github.com>
2024-06-05 13:22:52 -0700

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -5,6 +5,7 @@ import pickle
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import time
+st.set_page_config(page_title='تشخیص دیابت - RoboAi', layout='centered', page_icon='🤖')
 
 def load_model():
     with open('saved.pkl', 'rb') as file:
@@ -21,6 +22,20 @@ def show_page():
     st.write("<h2 style='text-align: center; color: gray;'>علائم خود را وارد کنید</h2>", unsafe_allow_html=True)
     st.write("<h4 style='text-align: center; color: gray;'>Robo-Ai.ir طراحی شده توسط</h4>", unsafe_allow_html=True)
     st.link_button("Robo-Ai بازگشت به", "https://robo-ai.ir")
+    with st.sidebar:
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.write(' ')
+        with col2:
+            st.image('img.png')
+        with col3:
+            st.write(' ')
+        st.divider()
+        st.write("<h4 style='text-align: right; color: gray;'>تشخیص دیابت با دقت 98 درصد</h>", unsafe_allow_html=True)
+        st.write("<h4 style='text-align: right; color: gray;'>ساخته شده با اطلاعات 420 کیس مبتلا به دیابت و سالم</h>", unsafe_allow_html=True)
+        st.divider()
+        st.write('Developed & Designed by')
+        st.write('Hamidreza Bahrami')
 
     Pregnancies = st.slider('تعداد بارداری', 0.0, 10.0, 1.0)
 
@@ -42,15 +57,48 @@ def show_page():
     if button:
         with st.chat_message("assistant"):
                 with st.spinner('''درحال بررسی، لطفا صبور باشید'''):
-                    time.sleep(3)
+                    time.sleep(2)
                     st.success(u'\u2713''بررسی انجام شد')
                     x = np.array([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
 
         y_prediction = model.predict(x)
         if y_prediction == 1:
-            st.write("<h4 style='text-align: right; color: gray;'>بر اساس داده های وارد شده، شما به دیابت مبتلا هستید</h4>", unsafe_allow_html=True)
-            st.write("<h5 style='text-align: right; color: gray;'>برای درمان به پزشک مراجعه کنید</h5>", unsafe_allow_html=True)
+            text1 = ' بر اساس تحلیل من ، شما به دیابت نوع 2 مبتلا هستید'
+            text2 = 'برای درمان به پزشک مراجعه کنید'
+            text3 = 'Based on my analysis, you are diagnosed with Type 2 Diabetes'
+            text4 = 'Please visit a doctor as soon as possible'
+            def stream_data1():
+                for word in text1.split(" "):
+                    yield word + " "
+                    time.sleep(0.09)
+            st.write_stream(stream_data1)
+            def stream_data2():
+                for word in text2.split(" "):
+                    yield word + " "
+                    time.sleep(0.09)
+            st.write_stream(stream_data2)
+            def stream_data3():
+                for word in text3.split(" "):
+                    yield word + " "
+                    time.sleep(0.09)
+            st.write_stream(stream_data3)
+            def stream_data4():
+                for word in text4.split(" "):
+                    yield word + " "
+                    time.sleep(0.09)
+            st.write_stream(stream_data4)
+            
         elif y_prediction == 0:
-            st.write("<h4 style='text-align: right; color: gray;'>بر اساس داده های وارد شده، شما در سلامتی کامل هستید</h4>", unsafe_allow_html=True)
-
+            text1 = ' بر اساس تحلیل من ، شما در سلامتی کامل هستید'
+            text2 = 'Based on my analysis, you are totally healthy'
+            def stream_data1():
+                for word in text1.split(" "):
+                    yield word + " "
+                    time.sleep(0.09)
+            st.write_stream(stream_data1)
+            def stream_data2():
+                for word in text2.split(" "):
+                    yield word + " "
+                    time.sleep(0.09)
+            st.write_stream(stream_data2)
 show_page()
